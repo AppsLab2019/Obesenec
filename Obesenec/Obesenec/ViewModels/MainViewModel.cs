199424057180afaa6f1b5c866aaea4ca19a889d4
openlab <openlab@z00p1-10.SPSKNM>
2020-02-20 08:06:48 +0100

    Add project files.

---
+++ Obesenec/Obesenec/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obesenec.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}
---
+++ Obesenec/Obesenec/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Obesenec.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Obesenec.Model
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly MainViewModel viewModel;
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            string[] Words = { "auto", "okno", "svet", "voda", "kryt" };
+            Random rnd = new Random();
+            int index = rnd.Next(Words.Length);
+            String s = Convert.ToString(Words[index]);
+            var chars = s.ToCharArray();
+            viewModel = new MainViewModel();
+            BindingContext = viewModel;
+
+            
+        }
+    }
+}
