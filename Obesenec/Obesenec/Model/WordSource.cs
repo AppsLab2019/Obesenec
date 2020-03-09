2932a925574947e51a5a039181661cc688982e28
MatusDurana <matusdurana2@gmail.com>
2020-03-09 10:16:03 +0100

    I make progress.

---
+++ Obesenec/Obesenec/Model/WordSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obesenec.Model
+{
+    class WordSource
+    {
+        private static readonly List<Word> words = new List<Word>()
+        {
+            new Word("auto"),
+            new Word("okno"),
+            new Word("svet"),
+            new Word("voda"),
+            new Word("kryt"),
+            new Word("pes"),
+
+        };
+
+        public static Word GetRandomWord()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(words.Count);
+            return words[index];
+        }
+
+    }
+}
--- Obesenec/Obesenec/ViewModels/MainViewModel.cs
+++ Obesenec/Obesenec/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Xamarin.Forms;
+using Obesenec.Model;
 
 namespace Obesenec.ViewModels
 {
@@ -12,14 +13,9 @@ namespace Obesenec.ViewModels
     {
         public MainViewModel()
         {
-            List = new ObservableCollection<Character>
-            {
-                new Character("V"),
-                new Character("L"),
-                new Character("A"),
-                new Character("D"),
-                new Character("O"),
-            };
+            var word = WordSource.GetRandomWord();
+            var characterList = word.GetListOfCharacters();
+            List = new ObservableCollection<Character>(characterList);
             CharacterPressedCommand = new Command<string>(CharacterPressed);
         }
 
@@ -48,56 +44,6 @@ namespace Obesenec.ViewModels
             {
                 // TODO
             }
-        }
-    }
-
-    public class Character : INotifyPropertyChanged
-    {
-        private string text;
-        private bool isShown;
-        private const string hiddenText = "_";
-
-        public Character(string text)
-        {
-            OriginalText = text;
-            Text = hiddenText;
-        }
-
-        public string OriginalText { get; }
-
-        public bool IsShown
-        {
-            get => isShown;
-            set
-            {
-                isShown = value;
-                if (isShown)
-                {
-                    Text = OriginalText;
-                }
-                else
-                {
-                    Text = hiddenText;
-                }
-            }
-        }
-
-        public string Text
-        {
-            get => text;
-            set
-            {
-                if (value == text) return;
-                text = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
